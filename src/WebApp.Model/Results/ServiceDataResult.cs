173751cddb81a31441af4211af5ea4ec7b2b14fd
Umay Eras <umayeras@hotmail.com>
2020-10-11 13:09:45 +0300

    create service results

---
+++ src/WebApp.Model/Results/ServiceDataResult.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApp.Model.Results
+{
+    public sealed class ServiceDataResult : ServiceResult
+    {
+        #region ctor
+
+        public object Data { get; set; }
+
+        public ServiceDataResult(bool isSuccess, string message, object data) : base(isSuccess, message)
+        {
+            Data = data;
+        }
+
+        #endregion
+
+        #region factory methods
+
+        public static ServiceDataResult Success(object data, string message = null)
+        {
+            return new ServiceDataResult(true, message, data);
+        }
+
+        public new static ServiceDataResult Error(string message = null)
+        {
+            return new ServiceDataResult(false, message, null);
+        }
+
+        #endregion
+    }
+}
---
+++ src/WebApp.Model/Results/ServiceResult.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApp.Model.Results
+{
+    public class ServiceResult
+    {
+        #region ctor
+
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public ServiceResult(bool isSuccess, string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+
+        #endregion
+
+        #region factory methods
+
+        public static ServiceResult Success(string message = null)
+        {
+            return new ServiceResult(true, message);
+        }
+
+        public static ServiceResult Error(string message = null)
+        {
+            return new ServiceResult(false, message);
+        }
+
+        #endregion
+    }
+}
