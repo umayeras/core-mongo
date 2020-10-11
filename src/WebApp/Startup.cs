d3d5393dbac55cd1f2a63f19e6747555ce5d52d9
Umay Eras <umayeras@hotmail.com>
2020-10-11 13:22:47 +0300

    Added Swagger

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using WebApp.Business.Abstract.Services;
 using WebApp.Business.Services;
 using WebApp.Data.Abstract.Factories;
@@ -29,6 +31,21 @@ namespace WebApp
             services.AddScoped<ISampleService, SampleService>();
             services.AddHealthChecks();
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Asp.Net Core WebApi with Mongo Db",
+                    Description = "A sample project for Asp.Net Core WebApi with Mongo Db",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Umay ERAS",
+                        Email = "umayeras@hotmail.com",
+                        Url = new Uri("http://umayeras.com"),
+                    }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -40,6 +57,11 @@ namespace WebApp
 
             app.UseHealthChecks("/health");
             app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
