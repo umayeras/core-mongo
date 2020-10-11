805a29db2df1f632241fb425b4f04e0b0f898983
Umay Eras <umayeras@hotmail.com>
2020-10-11 13:11:02 +0300

    Create SampleController.cs

---
+++ src/WebApp/Controllers/SampleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Business.Abstract.Services;
+using WebApp.Model.Entities;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        private readonly ISampleService sampleService;
+
+        public SampleController(ISampleService sampleService)
+        {
+            this.sampleService = sampleService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(sampleService.GetAll());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            return Ok(sampleService.Get(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Sample sample)
+        {
+            var result = sampleService.Add(sample);
+            return Ok(result.Message);
+        }
+    }
+}
